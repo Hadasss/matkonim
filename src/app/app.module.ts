31aa9262040522d76a8f5f4f1f4f14f1d872118e
Hadasss <hadas.gadish@gmail.com>
2023-07-07 16:06:12 -0700

    files and folders for app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShoppingItemComponent } from './shopping-list/shopping-item/shopping-item.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, ShoppingListComponent, RecipeBookComponent, ShoppingItemComponent, RecipeItemComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
