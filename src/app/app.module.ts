57c75d1d28cd06f02677e366402beae96f15282f
Hadasss <hadas.gadish@gmail.com>
2023-07-07 16:25:28 -0700

    modified file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,21 @@ import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { ShoppingItemComponent } from './shopping-list/shopping-item/shopping-item.component';
-import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ShoppingListComponent, RecipeBookComponent, ShoppingItemComponent, RecipeItemComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ShoppingItemComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
